c5e7db913c90682bd13491cbe740f1bbe1005177
SyedAnser <113206580+SyedAnser@users.noreply.github.com>
2023-09-02 12:24:56 +0530

    Delete factorial.py

--- factorial.py
+++
@@ -1,9 +0,0 @@
-a=int(input("Enter a number:"))
-for i in range(1,a):
-    a*=i
-print(a)
-
-
-#the normal method appears to be faster and more efficent. 
-#the amount of multiplication remains same in both methods but the cat of calling the 
-#same function over and over again makes recursion slower.
